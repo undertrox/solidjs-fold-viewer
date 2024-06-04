7621c227b4ea6827605135a1998ab543409b36c3
undertrox <undertrox@gmail.com>
2024-06-04 14:44:24 +0200

    optimize renderer by batching line draws

--- fold-viewer/src/FoldCanvasRenderer.ts
+++ fold-viewer/src/FoldCanvasRenderer.ts
@@ -1,42 +1,65 @@
+type Line = [number, number, number, number];
+
 export class FoldCanvasRenderer {
   private readonly canvas: HTMLCanvasElement;
   private readonly foldFile: FOLD;
   translation: [number, number] = [0,0];
   zoom: number = 1;
-  private zoomCenter: [number, number] = [0,0];
-
-  private colors: {[key: string]: string} = {
-    "M": "red",
-    "V": "blue",
-    "B": "black",
-    "F": "gray",
-  };
+  private mLines: Line[] = [];
+  private vLines: Line[] = [];
+  private bLines: Line[] = [];
 
   constructor(foldFile: FOLD, canvas: HTMLCanvasElement) {
     this.foldFile = foldFile;
     this.canvas = canvas;
+    if (!foldFile) {return;}
+    foldFile.edges_vertices.forEach(([i1, i2], index) => {
+      const [x1, y1] = this.foldFile.vertices_coords[i1];
+      const [x2, y2] = this.foldFile.vertices_coords[i2];
+      const line: Line = [x1, y1, x2, y2];
+      const col = this.foldFile.edges_assignment[index];
+      switch (col){
+        case "M":
+          this.mLines.push(line);
+          break;
+        case "V":
+          this.vLines.push(line);
+          break;
+        case "B":
+          this.bLines.push(line);
+          break;
+      }
+    })
   }
 
   public render() : void {
-    let ctx = this.canvas.getContext("2d")
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    if (this.foldFile == null) {return;}
-    ctx.save();
-    const [translateX, translateY] = this.translation;
-    ctx.scale(this.zoom, this.zoom);
-    ctx.translate(translateX, translateY);
-    let edges_vertices = this.foldFile.edges_vertices;
-    for (let i = 0; i < edges_vertices.length; i++) {
-      let [i1, i2] = edges_vertices[i];
-      const [x1, y1] = this.foldFile.vertices_coords[i1];
-      const [x2, y2] = this.foldFile.vertices_coords[i2];
-      ctx.strokeStyle = this.colors[this.foldFile.edges_assignment[i]];
-      ctx.beginPath();
+    requestAnimationFrame(() => {
+      let ctx = this.canvas.getContext("2d")
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+      if (this.foldFile == null) {return;}
+      ctx.save();
+      const [translateX, translateY] = this.translation;
+      ctx.scale(this.zoom, this.zoom);
+      ctx.translate(translateX, translateY);
+
+      this.renderLines(ctx, "blue", this.vLines);
+      this.renderLines(ctx, "red", this.mLines);
+      this.renderLines(ctx, "black", this.bLines);
+
+      ctx.restore();
+    });
+
+  }
+
+  private renderLines(ctx: CanvasRenderingContext2D, strokeStyle: string, lines: Line[]) : void {
+    ctx.strokeStyle = strokeStyle;
+    ctx.beginPath();
+    for (let i = 0; i < lines.length; i++) {
+      let [x1, y1, x2, y2] = lines[i];
       ctx.moveTo(x1, y1);
       ctx.lineTo(x2, y2);
-      ctx.stroke();
     }
-    ctx.restore();
+    ctx.stroke();
   }
 }
